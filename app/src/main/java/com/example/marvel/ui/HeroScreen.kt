d9e35de6bf8341146dc544845e7f06c59064607f
beehoney221 <mashatyu@mail.ru>
2024-07-04 01:15:28 +0600

    style: del import, ") {" -> "){"

--- app/src/main/java/com/example/marvel/ui/HeroScreen.kt
+++ app/src/main/java/com/example/marvel/ui/HeroScreen.kt
@@ -1,4 +1,5 @@
-import androidx.compose.foundation.Image
+package com.example.marvel.ui
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +32,7 @@ import com.example.marvel.data.Heroes
 fun HeroScreen(
     hero: Hero,
     onClick: () -> Unit
-) {
+){
     Box(modifier = Modifier.background(Color.Transparent)) {
         AsyncImage(
             model = ImageRequest.Builder(LocalContext.current)
@@ -43,13 +43,6 @@ fun HeroScreen(
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
-
-//        Image(
-//            painterResource(id = hero.id),
-//            contentDescription = hero.name,
-//            contentScale = ContentScale.Crop,
-//            modifier = Modifier.fillMaxSize()
-//        )
 
         IconButton(
             modifier = Modifier.padding(top = 5.dp, start = 5.dp),
@@ -70,8 +63,7 @@ fun HeroScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(start = 30.dp, bottom = 80.dp, end = 30.dp)
-        )
-        {
+        ){
             Column(
                 horizontalAlignment = Alignment.Start,
                 verticalArrangement = Arrangement.Bottom
