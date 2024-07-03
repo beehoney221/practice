fb691fa6f710965a2715ebb906c76739a5fdc606
beehoney221 <mashatyu@mail.ru>
2024-07-03 23:50:45 +0600

    refactor

--- app/src/main/java/com/example/marvel/MainActivity.kt
+++ app/src/main/java/com/example/marvel/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Surface
-import com.example.marvel.ui.ui.MarvelTheme
+import com.example.marvel.ui.theme.MarvelTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
